5c5677375206478b3be3a0888605b024e7e4b683
Sebastian Omar de la Vega <61508227+SebaGxT@users.noreply.github.com>
2020-06-11 20:42:54 -0300

    Actividad 6
    Correcciones

--- Ejercicio 9 - 25 duplas cancela con 0.cpp
+++ Ejercicio 9 - 25 duplas cancela con 0.cpp
@@ -8,119 +8,99 @@
 int main(void){
 	
 //Variables
-
-	const int DUP=25;
-	int i,a,b,veca[DUP],vecb[DUP],vecmay[DUP],v=0;
-
+	
+	const int N=25;
+	int i,a[N],b[N],r[N],cont;
+	
+	cont=0;
+	
 //Ingreso de datos	
 	
 	printf("Ingresar valores A y B.\n\n");
 	printf("Para cancelar la carga de valores ingresar un 0.\n\n");
 	
-	//Carga de ceros para eliminar números aleatorios en memoria
-	for(i=0;i<DUP;i++){
-		
-		veca[i]=0;
-		vecb[i]=0;
-		vecmay[i]=0;
-		
-	}
+	//Carga de valor A
+	printf("\nIngrese el valor de A: ");
+	scanf("%d",&a[cont]);
+	fflush(stdin);
 	
-	for(i=0;i<DUP;i++){
+	if(a[cont]!=0){
 		
-		//Carga de valor a en vector
-		printf("Ingresar un valor A: ");
-		scanf("%d",&a);
+		//Carga de valor B
+		printf("\nIngrese el valor de B: ");
+		scanf("%d",&b[cont]);
 		fflush(stdin);
 		
-		if(a!=0){
-			
-			veca[i]=a;
-
-			//Variable para impresion final
-			v++;
-	
-			//Carga de valor b en vector
-			printf("Ingresar un valor B: ");
-			scanf("%d",&b);
-			fflush(stdin);
-		
-			if(b!=0){
-			
-				vecb[i]=b;
-	
-			}else{
-			
-				printf("\n\nFinalizo la carga de valores. Se muestran los resultados.\n\n");
-				//Cancela el for
-				i=DUP;
-			
-			}
-			
-		}else if(i!=0){
-			
-			printf("\n\nFinalizo la carga de valores. Se muestran los resultados.\n\n");
-			//Cancela el for
-				i=DUP;
-	
-		}else{
-			
-			//Cancela el for en la primer vuelta
-			i=DUP;
-			
-		}
-		
 	}
+	
+	while((a[cont]!=0)&&(b[cont]!=0)&&(cont<N)){
+		
+		cont++;
+		
+		//Carga de valor A
+		printf("\nIngrese el valor de A: ");
+		scanf("%d",&a[cont]);
+		fflush(stdin);
+		
+		if(a[cont]!=0){
 			
+			//Carga de valor B
+			printf("\nIngrese el valor de B: ");
+			scanf("%d",&b[cont]);
+			fflush(stdin);
+	
+		}
+	}
+
 //Proceso
 	
-	//Verificar mayores menores e iguales
-	for(i=0;i<DUP;i++){
+	//Realiza el proceso en caso haber datos cargados previamente
+	if(cont>0){
 		
-		if(veca[i]>vecb[i]){
+		//Verificación de mayor, menor o igual
+		for(i=0;i<cont;i++){
 			
-			vecmay[i]=1;
+			if(a[i]>b[i]){
+				
+				r[i]=1;
+				
+			}else if(a[i]<b[i]){
+				
+				r[i]=-1;
+				
+			}else{
+				
+				r[i]=0;
+				
+			}
 			
-		}else if(vecb[i]>veca[i]){
+		}
+
+//Salida de datos
+		
+		for(i=0;i<cont;i++){
 			
-			vecmay[i]=2;
-			
-		}else if(veca[i]==vecb[i]){
-			
-			vecmay[i]=3;
+			//Listado de resultado
+			if(r[i]==1){
+				
+				printf("\n%d   %d Mayor",a[i],b[i]);
+				
+			}else if(r[i]==-1){
+				
+				printf("\n%d   %d Menor",a[i],b[i]);
+				
+			}else{
+				
+				printf("\n%d   %d Igual",a[i],b[i]);
+				
+			}
 			
 		}
 		
-	}
-	
-//Salida de datos
-	
-	if(veca[0]!=0){
-		
-		printf("\n\nLos resultados son: \n\n");
-	
-		for(i=0;i<v;i++){
-				
-				if(vecmay[i]==1){
-			
-					printf("El valor %d ingresado mayor es A: %d\n\n",i+1,veca[i]);
-			
-				}else if(vecmay[i]==2){
-			
-					printf("El valor %d ingresado mayor es B: %d\n\n",i+1,vecb[i]);
-			
-				}else if(vecmay[i]==3){
-			
-					printf("Los valores ingresados %d A y B son iguales: A: %d B:%d\n\n",i+1,veca[i],vecb[i]);
-			
-				}
-			}
-			
-		printf("\n\nNo se ingresaron m%cs valores.\n\n",160);
-	
 	}else{
 		
-		printf("\n\nNo se ingresaron valores, finaliza el programa.\n\n");
+		//Mensaje en caso de iniciación con 0
+		printf("\n\nSe finalizo el programa.");
 		
 	}
 	
@@ -131,6 +111,3 @@ int main(void){
 	return 0;
 		
 }
-
-
-
